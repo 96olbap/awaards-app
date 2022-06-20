9e2fbf3b8233738c5e11756ff9af79b1298e0a2a
96olbap <alvinwanjala@gmail.com>
2022-06-20 10:26:47 +0300

    config and urls

--- awaardsprj/urls.py
+++ awaardsprj/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.authtoken import views
+
+import awaards
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls')),
+    path('api-token-auth/', views.obtain_auth_token, name='api-token-auth'),
+    path('',include('awards.urls')),
 ]
